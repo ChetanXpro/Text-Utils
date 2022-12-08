dfe325e1993c8989a4a40f4649fb3f911db893c0
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-12-08 20:38:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Navbar from "./components/Navbar";
 import FormText from "./components/TextForm";
 
 function App() {
-  const [mode, setmode] = useState("light");
-
+  const [mode, setmode] = useState("light")
+//
   const togglemode = () => {
     if (mode === "light") {
       setmode("dark");
-      document.body.style.backgroundColor = "#4a4c4e";
+      document.body.style.backgroundColor = "#4a4c4e"
     } else {
       setmode("light");
       document.body.style.backgroundColor = "white";
